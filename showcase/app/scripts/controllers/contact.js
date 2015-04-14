ef0fa293de7eaee7367e8acd701f62160e9e0cb9
webdev <webdev@webDevVM>
2015-04-14 13:22:43 +0200

    add showcase

---
+++ showcase/app/scripts/controllers/contact.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name showcaseApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the showcaseApp
+ */
+angular.module('showcaseApp')
+  .controller('ContactCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $http.get("infos.json").success(function(data) {
+      $scope.modele = data;
+    });
+
+
+  });
